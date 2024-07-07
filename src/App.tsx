b098b87edae87a798927f99c77296d31043b8091
hasankarim18 <hallysust@gmail.com>
2024-07-07 23:04:07 +0600

    branch a rtk

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="bg-purple-300 ">
       <div className=" max-w-7xl min-h-screen bg-white mx-auto">
+        <h1 className="text-center border-b-lime-400 border">
+          Branch a: Todo with rtk query
+        </h1>
         main branch
       </div>
     </div>
