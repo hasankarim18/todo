483a4af3c819508e6f0bd0171d6eaf8fbcc7f5d0
hasankarim18 <hallysust@gmail.com>
2024-07-07 22:59:55 +0600

    main

---
+++ README.md
@@ -0,0 +1,3 @@
+## Project Target
+
+### # `Brand a` : Todo app with `rtk query`
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-purple-300 ">
+      <div className=" max-w-7xl min-h-screen bg-white mx-auto">
+        main branch
+      </div>
+    </div>
+  );
+}
+
+export default App;
